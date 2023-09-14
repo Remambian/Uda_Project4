5cd40b34a9e775683992b2e0da296e2d35e6343b
Anand <remambian@gmail.com>
2023-09-14 19:20:56 +0530

    Update docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -4,12 +4,16 @@
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+Create dockerpath
+dockerpath=UdacityDocker/app/
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+sudo docker login $registry -u remambian@gmail.com -p Udacity@2023
+
 # Step 3:
 # Push image to a docker repository
+
+sudo docker push UdacityDocker/app/
