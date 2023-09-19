08834fbd42ed9c9e1a30221d4308c341845b3d90
Anand <remambian@gmail.com>
2023-09-19 12:26:39 +0530

    update kubernetees

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=remambian/app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment kubernetes-udacityproj --image=remambian/app
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/kubernetes-udacityproj-6ff5cf665-hnf9w --address 0.0.0.0 8000:80
